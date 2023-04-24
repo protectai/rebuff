c14d1bab214b9e9e4333afcd735458bb022b7219
Willem Pienaar <github@willem.co>
2023-04-23 22:48:50 -0700

    Removed tailwind and added UI

--- server/pages/_app.tsx
+++ server/pages/_app.tsx
@@ -1,18 +1,35 @@
-import { supabase } from '@/lib/supabase'
-import '@/styles/app.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import type { AppProps } from 'next/app'
-import {useState} from "react";
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { supabase } from '@/lib/supabase';
+// import '@/styles/app.css';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps<{
-    initialSession: Session
-}>) {
-    const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+export default function App(props: AppProps<{ initialSession: Session }>) {
+    const { Component, pageProps } = props;
+    const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
     return (
-        <SessionContextProvider supabaseClient={supabaseClient}
-                                initialSession={pageProps.initialSession}>
-          <Component {...pageProps} />
-        </SessionContextProvider>
-    )
+        <>
+            <Head>
+                <title>Page title</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: 'light',
+                }}
+            >
+                <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+                    <Component {...pageProps} />
+                </SessionContextProvider>
+            </MantineProvider>
+        </>
+    );
 }
--- server/pages/_document.tsx
+++ server/pages/_document.tsx
@@ -1,20 +1,22 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { HeaderAction } from "@/pages/_header";
+import { Header } from "@mantine/core";
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-    static getInitialProps = getInitialProps;
+  static getInitialProps = getInitialProps;
 
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
