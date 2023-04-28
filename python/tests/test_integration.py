a36702de3e570834fc7b83a72f5a4a6b822e45be
Willem Pienaar <github@willem.co>
2023-04-27 21:29:54 -0700

    feat(test_integration.py): add fixture to manage Next.js server's lifecycle and start it before running tests.

--- python/tests/test_integration.py
+++ python/tests/test_integration.py
@@ -1,5 +1,41 @@
-import pytest
+import os
+import sys
+
 from rebuff import Rebuff
+
+try:
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../rebuff"))
+    )
+except NameError:
+    pass
+import subprocess
+import time
+import pytest
+
+
+# Define a fixture to manage the Next.js server's lifecycle
+@pytest.fixture(scope="session", autouse=True)
+def nextjs_server():
+    # Get the absolute path to the root of the Git repository
+    git_root = subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"],
+        capture_output=True,
+        text=True,
+        check=True
+    ).stdout.strip()
+
+    # Start the Next.js server as a subprocess
+    server = subprocess.Popen(["npm", "run", "dev"], cwd=f"{git_root}/server")
+
+    # Wait for the server to start (adjust the sleep time as needed)
+    time.sleep(5)
+
+    # Yield control to the test function
+    yield
+
+    # Stop the server subprocess after the tests are completed
+    server.terminate()
 
 def test_integration():
     # Initialize the Rebuff SDK with a real API token and URL
