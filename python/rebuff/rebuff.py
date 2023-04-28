387a68961925c5eb4a5cf6c5dfed17651df82388
Willem Pienaar <github@willem.co>
2023-04-27 21:06:37 -0700

    feat(rebuff.py): add is_injection_detected method to check user input for injection using API call.

--- python/rebuff/rebuff.py
+++ python/rebuff/rebuff.py
@@ -1,10 +1,20 @@
+import requests
+
 class Rebuff:
     def __init__(self, api_token, api_url):
         self.api_token = api_token
+        self.api_url = api_url
 
     def is_injection_detected(self, user_input):
-        # Implement the logic to detect prompt injection.
-        pass
+        headers = {
+            "Authorization": f"Bearer {self.api_token}"
+        }
+        data = {
+            "user_input": user_input
+        }
+        response = requests.post(self.api_url, json=data, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
     def add_canaryword(self, prompt_template):
         # Implement the logic to add canary word to the prompt.
