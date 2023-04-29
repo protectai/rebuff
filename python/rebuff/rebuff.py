8ae87f7be60c090d011d1c7dbf8df091d1a36c0e
Willem Pienaar <github@willem.co>
2023-04-29 11:40:43 -0700

    feat(rebuff.py): add generate_canary_word, add_canaryword, is_canaryword_leaked, and log_leakage methods to Rebuff class

--- python/rebuff/rebuff.py
+++ python/rebuff/rebuff.py
@@ -1,3 +1,4 @@
+import secrets
 from dataclasses import dataclass
 from typing import Union
 
@@ -35,7 +36,9 @@ class Rebuff:
         self.api_token = api_token
         self.api_url = api_url
 
-    def is_injection_detected(self, user_input: str, check_heuristic: bool = True, check_vector: bool = True, check_llm: bool = True, vector_similarity: float = 0.9) -> Union[DetectApiSuccessResponse, DetectApiFailureResponse]:
+    def is_injection_detected(self, user_input: str, check_heuristic: bool = True, check_vector: bool = True,
+                              check_llm: bool = True, vector_similarity: float = 0.9) -> \
+            Union[DetectApiSuccessResponse, DetectApiFailureResponse]:
         headers = {
             "Authorization": f"Bearer {self.api_token}",
             "Content-Type": "application/json"
@@ -60,6 +63,27 @@ class Rebuff:
         success_response = DetectApiSuccessResponse.parse_obj(response_json)
         return success_response
 
+    def generate_canary_word(self, length: int = 8) -> str:
+        # Generate a secure random hexadecimal canary word
+        return secrets.token_hex(length // 2)
+
+    def add_canaryword(self, prompt_template: str, canary_word: Optional[str] = None, canary_format: str = "<!-- {canary_word} -->") -> Tuple[str, str]:
+        # Generate a canary word if not provided
+        if canary_word is None:
+            canary_word = self.generate_canary_word()
+        # Embed the canary word in the specified format
+        canary_comment = canary_format.format(canary_word=canary_word)
+        # Append the canary comment to the original prompt
+        prompt_with_canary = prompt_template + "\n" + canary_comment
+        return canary_word, prompt_with_canary
+
+    def is_canaryword_leaked(self, completion: str, canary_word: str) -> bool:
+        # Check if the canary word appears in the completion
+        return canary_word in completion
+
+    def log_leakage(self, user_input: str, templated_prompt: str, response: str, canary_word: str):
+        pass
+
     def detect_leakage(self, output_text, canary_word):
         headers = {"Authorization": f"Bearer {self.api_token}"}
         data = {
@@ -70,9 +94,3 @@ class Rebuff:
         response = requests.post(self.api_url, json=data, headers=headers)
         response.raise_for_status()
         return response.json()
-
-    def add_canaryword(self, prompt_template):
-        pass
-
-    def is_canaryword_leaked(self, completion, canary_word):
-        pass
