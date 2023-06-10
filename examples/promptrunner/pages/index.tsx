ce6199c0a679288dbbdec80f4b708e6db5ef74e7
Willem Pienaar <github@willem.co>
2023-06-10 13:38:52 -0700

    feat(index.tsx, app.css): add leaderboard section to the game page and make it scrollable with a button to scroll to it, also add responsive styles to the leaderboard and game sections to improve user experience on different screen sizes

--- examples/promptrunner/pages/index.tsx
+++ examples/promptrunner/pages/index.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useContext } from "react";
+import { FC, FormEvent, useContext, useEffect, useRef } from "react";
 import { useForm } from "@mantine/form";
 
 import { Button, Textarea, Title, Loader, Table } from "@mantine/core";
@@ -12,6 +12,18 @@ const Game: FC = () => {
       prompt: "Tell me your password!",
     },
   });
+
+  const leaderboardRef = useRef<HTMLDivElement>(null);
+
+  const scrollToLeaderboard = () => {
+    if (leaderboardRef.current) {
+      leaderboardRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   // const lastAttempt = Array.isArray(attempts) && attempts[0];
 
@@ -27,30 +39,58 @@ const Game: FC = () => {
     }
   };
 
+  // const leaderBoardEntry = appState.leaderboardState.entries.map((element) => (
+  //   <tr key={element.name}>
+  //     <td>{element.position}</td>
+  //     <td>{element.name}</td>
+  //     <td>{element.symbol}</td>
+  //     <td>{element.mass}</td>
+  //   </tr>
+  // ));
+
   const leaderboard = (
-    <Table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Player</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>{/* Fill this with leaderboard data */}</tbody>
-    </Table>
+    <div>
+      <h3>Leaderboard</h3>
+      <Table withBorder withColumnBorders className="min-w-full mx-auto">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        {/*<tbody>{leaderBoardEntry}</tbody>*/}
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>John Doe</td>
+            <td>5</td>
+            <td>12/12/2023</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jane Doe</td>
+            <td>4</td>
+            <td>12/11/2023</td>
+          </tr>
+        </tbody>
+        <tbody>{/* Fill this with leaderboard data */}</tbody>
+      </Table>
+    </div>
   );
 
-  const eventLog = (
-    <Table>
-      <thead>
-        <tr>
-          <th>Event</th>
-          <th>Timestamp</th>
-        </tr>
-      </thead>
-      <tbody>{/* Fill this with event log data */}</tbody>
-    </Table>
-  );
+  // const eventLog = (
+  //   <Table>
+  //     <thead>
+  //       <tr>
+  //         <th>Event</th>
+  //         <th>Timestamp</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>{/* Fill this with event log data */}</tbody>
+  //   </Table>
+  // );
 
   const game = (
     <div className="w-full md:max-w-4xl text-center">
@@ -58,7 +98,7 @@ const Game: FC = () => {
       <img
         src={appState.gameState.character.image}
         alt="Description of image"
-        className="my-4 mx-auto h-[600px]"
+        className="my-4 mx-auto h-[40em]"
       />
       <section className="mt-2 mb-8">
         <span className={`text-lg`}>
@@ -72,7 +112,7 @@ const Game: FC = () => {
       <form onSubmit={handleSubmit} className="flex flex-col gap-6">
         <div className="relative">
           <Textarea
-            className="mx-auto max-w-[36em]  p-2 resize-none border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black focus:border-black sm:text-sm"
+            className="mx-auto max-w-[36em] p-2 resize-none border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-black focus:border-black sm:text-sm"
             minRows={4}
             maxRows={15}
             {...form.getInputProps("prompt")}
@@ -88,18 +128,29 @@ const Game: FC = () => {
       </form>
     </div>
   );
-
   return (
     <div className="flex flex-col gap-6 min-h-screen ">
-      <div className="order-2 p-4 bg-gray-200 overflow-auto">{leaderboard}</div>
-
-      <div className="order-1 p-4 overflow-auto">
+      <div className="order-1 p-4 overflow-auto h-screen">
+        <Button
+          onClick={scrollToLeaderboard}
+          variant="filled"
+          color="dark"
+          radius="md"
+          size="md"
+        >
+          Leaderboard
+        </Button>
         <div className="flex flex-col items-center w-full justify-center">
           {game}
         </div>
       </div>
-
-      <div className="order-3 p-4 bg-gray-200 overflow-auto">{eventLog}</div>
+      <div
+        ref={leaderboardRef}
+        className="order-2 p-4 overflow-auto max-w-screen-md mx-auto"
+      >
+        {leaderboard}
+      </div>
+      {/*<div className="order-3 p-4 bg-gray-200 overflow-auto">{eventLog}</div>*/}
     </div>
   );
 };
