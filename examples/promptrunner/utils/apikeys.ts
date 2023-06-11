2b13170de6e5bc855bceb4f4622ab1df633a5de5
Willem Pienaar <github@willem.co>
2023-06-10 21:37:32 -0700

    chore(apikeys.ts): remove unused apikeys module as it is no longer needed

--- examples/promptrunner/utils/apikeys.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from "crypto";
-
-export function generateApiKey(length: number = 64): string {
-  // Generate half the number of bytes, since each byte is represented by two hexadecimal characters
-  const numBytes = Math.ceil(length / 2);
-  const apiKey = randomBytes(numBytes).toString("hex");
-  // Truncate the result to the desired length in case of odd length values
-  return apiKey.substring(0, length);
-}
