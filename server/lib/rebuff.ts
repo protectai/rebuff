8735e3af56872750e17daea7f79c82b2b60d7b3e
Risto McGehee <risto.mcgehee@gmail.com>
2024-01-14 14:17:58 -0800

    Add server support for chroma

--- server/lib/rebuff.ts
+++ server/lib/rebuff.ts
@@ -1,14 +1,28 @@
-import { RebuffSdk } from "rebuff";
+import { RebuffSdk, VectorDbConfig } from "rebuff";
 import { getEnvironmentVariable } from "./general-helpers";
+
+let vectorDB: VectorDbConfig;
+if (process.env["VECTOR_DB"] === "chroma") {
+  vectorDB = {
+    chroma: {
+      url: getEnvironmentVariable("CHROMA_URL"),
+      collectionName: getEnvironmentVariable("CHROMA_COLLECTION_NAME"),
+    }
+  }
+} else {
+  vectorDB = {
+    pinecone: {
+      environment: getEnvironmentVariable("PINECONE_ENVIRONMENT"),
+      apikey: getEnvironmentVariable("PINECONE_API_KEY"),
+      index: getEnvironmentVariable("PINECONE_INDEX_NAME"),
+    }
+  }
+}
 
 export const rebuff = new RebuffSdk({
   openai: {
     apikey: getEnvironmentVariable("OPENAI_API_KEY"),
     model: "gpt-3.5-turbo",
   },
-  pinecone: {
-    environment: getEnvironmentVariable("PINECONE_ENVIRONMENT"),
-    apikey: getEnvironmentVariable("PINECONE_API_KEY"),
-    index: getEnvironmentVariable("PINECONE_INDEX_NAME"),
-  },
+  vectorDB: vectorDB,
 });
